70ad7186af08abd7363eccba1ee6d0a39e99ba7b
venkat77777 <venkat@appzoy.com>
2019-12-16 18:48:24 +0530

    committed client changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { ApolloProvider } from "@apollo/react-hooks";
+
 import AppNavigation from './framework/AppNavigation';
 import 'react-native-gesture-handler';
 import {AuthProvider} from './context/AuthProvider';
+import apolloClient from './Adapters/getClient';
 
 const App = () => {
   return (
-    <AuthProvider>
-      <AppNavigation />
-    </AuthProvider>
+    <ApolloProvider client={apolloClient}>
+      <AuthProvider>
+        <AppNavigation/>
+      </AuthProvider>
+    </ApolloProvider>
   );
 };
 
